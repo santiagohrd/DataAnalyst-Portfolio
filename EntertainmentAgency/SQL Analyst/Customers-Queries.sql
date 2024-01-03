e55ae71d5ee603f8ad0a9b8b76d52dd801d3ad00
SantiagoHrd <santiagohrd1@gmail.com>
2024-01-02 22:32:46 -0500

    Add queries about most music style, entertainers required, and cities, and clients by cities

---
+++ EntertainmentAgency/SQL Analyst/Customers-Queries.sql
@@ -0,0 +1,33 @@
+USE EntertainmentAgency;
+
+--Getting to know the tables and columns of the database
+SELECT table_name, column_name, data_type, character_maximum_length, is_nullable
+FROM INFORMATION_SCHEMA.COLUMNS;
+
+--most popular music styles
+select StyleName, mp.PreferenceSeq
+from Musical_Styles as ms
+inner join Musical_Preferences as mp
+on ms.StyleID = mp.StyleID
+where mp.PreferenceSeq = (
+	select max(PreferenceSeq) from Musical_Preferences);
+
+--Entertainments most required
+select e.EntStageName, count(eg.EngagementNumber) as NumEngagements
+from Entertainers as e
+inner join Engagements as eg
+	on eg.EntertainerID = e.EntertainerID
+group by e.EntStageName
+order by NumEngagements desc;
+
+--top three cities with the most engaged customers
+select top(3) c.CustState, c.CustCity, count(EngagementNumber) as NumEngagements
+from customers as c
+inner join Engagements as e
+	on e.CustomerID = c.CustomerID
+group by c.CustState, c.CustCity
+order by NumEngagements desc;
+
+select CustCity, COUNT(CustCity) as NumMembers
+from Customers
+group by CustCity;
