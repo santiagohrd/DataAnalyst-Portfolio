9f0163825fb523b6004d4c14c7cac23a0aa4d25c
SantiagoHrd <santiagohrd1@gmail.com>
2024-01-09 06:10:56 -0500

    Add average contract price for engagements in each month

--- EntertainmentAgency/SQL Analyst/Agents Queries.sql
+++ EntertainmentAgency/SQL Analyst/Agents Queries.sql
@@ -11,6 +11,23 @@ where e.StartDate >= dateadd(month, -3, StartDate)
 group by CONCAT(a.agtfirstname, ' ', a.AgtLastName)
 order by maxContractPrice desc;
 
+-- average for the last quarters  
+select case
+		when MONTH(StartDate) in (9, 10, 11) then '1st Quarter'
+        when MONTH(StartDate) in (12, 1, 2) then '2nd Quarter'
+        when MONTH(StartDate) in (3, 4, 5) then '3rd Quarter'
+        else '4th Quarter'
+	end as Quarters,
+round(avg(contractprice), 2) as avgContractPrice
+from Engagements
+group by case
+		when MONTH(StartDate) in (9, 10, 11) then '1st Quarter'
+        when MONTH(StartDate) in (12, 1, 2) then '2nd Quarter'
+        when MONTH(StartDate) in (3, 4, 5) then '3rd Quarter'
+        else '4th Quarter'
+	end
+order by quarters;
+
 --closed engagements
 select CONCAT(a.agtfirstname, ' ', a.AgtLastName) as agent, COUNT(e.engagementnumber) as TotalEngagements
 from Agents as a
@@ -42,19 +59,34 @@ where e.StartDate between dateadd(month, -3, StartDate)
 	and e.StartDate
 group by CONCAT(agtfirstname, ' ', AgtLastName);
 
--- average for the last quarters  
+--average contract price for engagements in each month
 select case
-		when MONTH(StartDate) in (9, 10, 11) then '1st Quarter'
-        when MONTH(StartDate) in (12, 1, 2) then '2nd Quarter'
-        when MONTH(StartDate) in (3, 4, 5) then '3rd Quarter'
-        else '4th Quarter'
-	end as Quarters,
-round(avg(contractprice), 2) as avgContractPrice
+		when month(StartDate) = 1 then 'Jan'
+		when month(StartDate) = 2 then 'Feb'
+		when month(StartDate) = 3 then 'Mar'
+		when month(StartDate) = 4 then 'Apr'
+		when month(StartDate) = 5 then 'May'
+		when month(StartDate) = 6 then 'Jun'
+		when month(StartDate) = 7 then 'Jul'
+		when month(StartDate) = 8 then 'Aug'
+		when month(StartDate) = 9 then 'Sep'
+		when month(StartDate) = 10 then 'Oct'
+		when month(StartDate) = 11 then 'Nov'
+		else 'Dec'
+	end as 'Month',
+avg(contractprice) as avgContractPrice
 from Engagements
 group by case
-		when MONTH(StartDate) in (9, 10, 11) then '1st Quarter'
-        when MONTH(StartDate) in (12, 1, 2) then '2nd Quarter'
-        when MONTH(StartDate) in (3, 4, 5) then '3rd Quarter'
-        else '4th Quarter'
-	end
-order by quarters;
+		when month(StartDate) = 1 then 'Jan'
+		when month(StartDate) = 2 then 'Feb'
+		when month(StartDate) = 3 then 'Mar'
+		when month(StartDate) = 4 then 'Apr'
+		when month(StartDate) = 5 then 'May'
+		when month(StartDate) = 6 then 'Jun'
+		when month(StartDate) = 7 then 'Jul'
+		when month(StartDate) = 8 then 'Aug'
+		when month(StartDate) = 9 then 'Sep'
+		when month(StartDate) = 10 then 'Oct'
+		when month(StartDate) = 11 then 'Nov'
+		else 'Dec'
+	end;
